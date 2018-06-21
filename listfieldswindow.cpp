6b171df152639f49366c75e78ef02521d5806b02
carlosvhz <carlosvhz@outlook.com>
2018-06-21 12:11:57 -0600

    Commit

--- listfieldswindow.cpp
+++ listfieldswindow.cpp
@@ -20,12 +20,14 @@ void listfieldswindow::filltable(File* file){
     ui->tableWidget->setHorizontalHeaderLabels(headers);
     ui->tableWidget->setRowCount(file->getFields().size);
     for(int i=1; i<=file->getFields().size; i++){
-        ui->tableWidget->setItem(i-1,0,new QTableWidgetItem(QString::fromStdString(file->getFields().get(i).getName())));//file->getFields().get(i).getName()
-        if(file->getFields().get(i).getType()==0) ui->tableWidget->setItem(i-1,1,new QTableWidgetItem("Integer"));//file->getFields().get(i).getType()
+        ui->tableWidget->setItem(i-1,0,new QTableWidgetItem(QString::fromStdString(file->getFields().get(i).getName())));
+        if(file->getFields().get(i).getType()==0) ui->tableWidget->setItem(i-1,1,new QTableWidgetItem("Integer"));
         else if(file->getFields().get(i).getType()==1) ui->tableWidget->setItem(i-1,1,new QTableWidgetItem("Character"));
         else if(file->getFields().get(i).getType()==2) ui->tableWidget->setItem(i-1,1,new QTableWidgetItem("String"));
-        if(file->getFields().get(i).isPrimaryKey()) ui->tableWidget->setItem(i-1,2,new QTableWidgetItem("Yes"));//file->getFields().get(i).isPrimaryKey()
+        if(file->getFields().get(i).isPrimaryKey()) ui->tableWidget->setItem(i-1,2,new QTableWidgetItem("Yes"));
         else ui->tableWidget->setItem(i-1,2,new QTableWidgetItem("No"));
         ui->tableWidget->setItem(i-1,3,new QTableWidgetItem(QString::number(file->getFields().get(i).getSize())));
     }
+    file = 0;
+    delete file;
 }
