2de41bed3d2d94663e2679e29e2f5414c5571473
carlosvhz <carlosvhz@outlook.com>
2018-06-22 13:48:08 -0600

    HasPrimaryKeu()

--- addfieldwindow.cpp
+++ addfieldwindow.cpp
@@ -20,17 +20,17 @@ addfieldwindow::~addfieldwindow()
 
 void addfieldwindow::on_pushButton_addfield_clicked()
 {
-    int type =0;
-    QString text= ui->lineEdit_name->text();
-    //Tipo 0 int 1 char 2 string, nombre, tamaño, es llave primaria
-    if(ui->comboBox_type->currentText()=="Character") type = 1;
-    else if(ui->comboBox_type->currentText()=="String") type = 2;
-    //file->addField(type,ui->lineEdit_name->text(),ui->spinBox_size->value(), false);
-    file->addField(type,text.toStdString(),ui->spinBox_size->value(), ui->radioButton_ispk->isChecked());
-    ui->radioButton_ispk->setChecked(false);
-    ui->lineEdit_name->setText("");
-    ui->comboBox_type->setCurrentIndex(0);
-    ui->spinBox_size->setValue(0);
+    if(!file->isLocked()){
+        int type =0;
+        QString text= ui->lineEdit_name->text();
+        if(ui->comboBox_type->currentText()=="Character") type = 1;
+        else if(ui->comboBox_type->currentText()=="String") type = 2;
+        file->addField(type,text.toStdString(),ui->spinBox_size->value(), ui->radioButton_ispk->isChecked());
+        ui->radioButton_ispk->setChecked(false);
+        ui->lineEdit_name->setText("");
+        ui->comboBox_type->setCurrentIndex(0);
+        ui->spinBox_size->setValue(0);
+    }
 }
 
 void addfieldwindow::setFile(File* con){
