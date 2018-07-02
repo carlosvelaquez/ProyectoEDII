686dd6bf4f1bab1696592f0fdb51b2c3eaee6c90
carlosvhz <carlosvhz@outlook.com>
2018-07-01 21:42:52 -0600

    modificar

--- modifyrecordwindow.h
+++ modifyrecordwindow.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include "File.h"
+#include "List.h"
 
 namespace Ui {
 class modifyrecordwindow;
@@ -18,9 +19,16 @@ public:
     void filltable();
     void setFile(File*);
 
+private slots:
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_clicked();
+
 private:
     Ui::modifyrecordwindow *ui;
     File* file;
+    List<string> record;
+    int indexRecord;
 };
 
 #endif // MODIFYRECORDWINDOW_H
